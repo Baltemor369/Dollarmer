3eb2d6af3e2376173b03edce60b22fef3378f80b
Baltemor369 <thomespo@gmail.com>
2024-05-26 15:58:36 +0200

    update

--- components/CButton.java
+++ components/CButton.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package components;
 
 import javax.swing.*;
@@ -36,42 +35,3 @@ public class CButton {
         return button;
     }
 }
-=======
-package components;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class CButton {
-    private JButton button;
-
-    public CButton(String text, Dimension size, Color backgroundColor, Color textColor, Font font) {
-        button = new JButton(text);
-
-        if (size != null) {
-            button.setPreferredSize(size);
-            button.setMinimumSize(size);
-            button.setMaximumSize(size);
-        }
-
-        if (backgroundColor != null) {
-            button.setBackground(backgroundColor);
-        }
-
-        if (textColor != null) {
-            button.setForeground(textColor);
-        }
-
-        if (font != null) {
-            button.setFont(font);
-        }
-
-        button.setBorderPainted(false);
-        button.setMargin(new Insets(0, 0, 0, 0));
-    }
-
-    public JButton getButton() {
-        return button;
-    }
-}
->>>>>>> origin/main
