91139303bb7b628df18467065d8628b964aef5fb
Baltemor369 <thomespo@gmail.com>
2024-06-20 14:59:03 +0200

    update activity class with new player stats

--- core/Activity.java
+++ core/Activity.java
@@ -8,11 +8,16 @@ import static core.Const.*;
 
 public class Activity {
     String _name;
+    // duration
     private int _hour, _min, _sec;
+    // access job time
     private int _hourStart, _hourEnd;
+    // 
     private int _salary, _exhaustion;
+    // skills requirements
+    private int _technical, _artistic, _communication, _science;
 
-    public Activity(String name, int hour, int minute, int salary, int exhaustion, int startHour, int endHour) {
+    public Activity(String name, int hour, int minute, int salary, int exhaustion, int startHour, int endHour,int technical,int artistic,int communication,int science) {
         this._name = name;
         this._hour = hour;
         this._min = minute;
@@ -20,43 +25,35 @@ public class Activity {
         this._exhaustion = exhaustion;
         this._hourStart = startHour;
         this._hourEnd = endHour;
+        this._technical = technical;
+        this._artistic = artistic;
+        this._communication = communication;
+        this._science = science;
     }
 
     @Override
-    public String toString(){
-        return _name+" exhaust:"+_exhaustion+" schedules:"+_hourStart+"-"+_hourEnd;
-    }
+    public String toString() { return _name + " exhaust:" + _exhaustion + " schedules:" + _hourStart + "-" + _hourEnd; }
 
-    public String getName() {
-        return _name;
-    }
-    public int getHour() {
-        return _hour;
-    }
-    public int getMinute() {
-        return _min;
-    }
-    public int getSecond() {
-        return _sec;
-    }
-    public int getHourStart() {
-        return _hourStart;
-    }
-    public int getHourEnd() {
-        return _hourEnd;
-    }
-    public int getSalary() {
-        return _salary;
-    }
-    public int getExhaust() {
-        return _exhaustion;
-    }
-    public int getTimeInMin(){
-        return _hour * 60 + _min; 
-    }
-    public int getTimeInSec(){
-        return _hour * 60 * 60 + _min * 60 + _sec; 
-    }
+    public String getName() { return _name; }
+    // getter time
+    public int getHour() { return _hour; }
+    public int getMinute() { return _min; }
+    public int getSecond() { return _sec; }
+    public int getHourStart() { return _hourStart; }
+    public int getHourEnd() { return _hourEnd; }
+    public int getTimeInMin() { return _hour * 60 + _min; }
+    public int getTimeInSec() { return _hour * 60 * 60 + _min * 60 + _sec; }
+
+    // 
+    public int getSalary() { return _salary; }
+    public int getExhaust() { return _exhaustion; }
+
+    // getter skils
+    public int getTechnical() {return _technical;}
+    public int getArtistic() {return _artistic;}
+    public int getCommunication() {return _communication;}
+    public int getScience() {return _science;}
+
     public JButton createActivityButton(ActionListener action) {
         JButton workButton = WButton.createButton(
             getName(), 
