717a5424a3d168acc25e562485d2a982f958ec7d
Rikudo4485 <thomespo@gmail.com>
2024-06-23 00:32:11 +0200

    remake run&compile script

--- run.bat
+++ run.bat
@@ -1,12 +1,4 @@
 @echo off
 
-IF EXIST bin (
-    echo project compilation
-    javac -d bin App.java core/*.java widgets/*.java
-)
-IF "%~1"=="-c" (
-    echo project compilation
-    javac -d bin App.java core/*.java widgets/*.java
-)
-echo app launch
+echo launch app
 java -cp bin App
